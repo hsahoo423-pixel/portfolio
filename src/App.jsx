66777c9462343d3de172f073d206a52119077d0a
Harapriya Sahoo <harapriya123@gmail.com>
2026-06-20 17:50:53 +0530

    Fixed Achievement component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Achievment";
 import Footer from "./components/Footer";
 import Achievment from "./components/Achievment";
 
